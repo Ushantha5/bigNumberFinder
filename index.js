669dba81d9540b68717ff6647bdb99a9663c6d58
ushantha <ushanthamr@gmail.com>
2025-09-04 15:58:32 +0530

    sffdw

---
+++ index.js
@@ -0,0 +1,15 @@
+function main(even) {
+    const a = parseFloat(document.getElementById)('a').value;
+    const b = parseFloat(document.getElementById)('b').value;
+    const c = parseFloat(document.getElementById)('c').value;
+if (a===b && b==c) {
+        result= `a,b,c are equal and biggest`
+
+    document.getElementById("output").innerHTML = result;
+
+
+
+   
+}
+   
+    
